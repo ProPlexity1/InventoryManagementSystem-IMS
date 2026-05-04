8a3c91680dd88f4c0b1075ba05d7108c7e64c6a9
ProPlexity1 <ukashaworkmail@gmail.com>
2026-05-04 12:51:29 +0500

    add railway config

--- InventoryAPI/obj/Debug/net9.0/InventoryAPI.AssemblyInfo.cs
+++ InventoryAPI/obj/Debug/net9.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e035deb47d95b3df57cee391dfa479b7cab7dca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cd27aeb2a331d35efa36302068f073fdcabeb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
