98ddff406105b2504c52b9eaccbc8ee0727deb74
ProPlexity1 <ukashaworkmail@gmail.com>
2026-06-21 11:03:27 +0500

    changed name

--- InventoryAPI/obj/Debug/net9.0/InventoryAPI.AssemblyInfo.cs
+++ InventoryAPI/obj/Debug/net9.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c7ff0d30a52f283e4afd53850b88bad07dc089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af37cfd17368032bfd41a882af2a662264d2621f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
